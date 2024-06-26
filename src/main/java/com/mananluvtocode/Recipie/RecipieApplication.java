d9605809c1dd5c461d1e0b51ec7dcc45a852a5d4
AnonymousManan <mananaggarwal2001@gmail.com>
2024-06-26 11:12:06 +0530

    intial commit

---
+++ src/main/java/com/mananluvtocode/Recipie/RecipieApplication.java
@@ -0,0 +1,13 @@
+package com.mananluvtocode.Recipie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipieApplication.class, args);
+	}
+
+}
