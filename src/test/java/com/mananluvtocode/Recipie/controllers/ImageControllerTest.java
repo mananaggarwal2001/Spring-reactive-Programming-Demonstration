8fa5f00234b24e074b19eb6ef7d1b06dfd202e52
AnonymousManan <mananaggarwal2001@gmail.com>
2024-07-15 12:33:13 +0530

    changes made in the file

--- src/test/java/com/mananluvtocode/Recipie/controllers/ImageControllerTest.java
+++ src/test/java/com/mananluvtocode/Recipie/controllers/ImageControllerTest.java
@@ -1,6 +1,4 @@
 package com.mananluvtocode.Recipie.controllers;
-
-import com.mananluvtocode.Recipie.commands.RecipeCommand;
 import com.mananluvtocode.Recipie.domain.Recipe;
 import com.mananluvtocode.Recipie.service.ImageService;
 import com.mananluvtocode.Recipie.service.RecipeService;
@@ -12,14 +10,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class ImageControllerTest {
